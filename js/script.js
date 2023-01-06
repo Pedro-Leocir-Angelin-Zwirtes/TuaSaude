6f70f1e9227ebfd1f0a93f2a22a590d205c96c0a
PedroLeocir <pedroangelinzwirtes@gmail.com>
2023-01-06 01:00:21 -0300

    Atualizado arredondamento de valores no JS, criado página contatos

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function calcWater() {
     var result1 = parseFloat(document.getElementById("value1").value)
     
-    var litros_por_dia = parseFloat(result1 * 0.035)
-    var mililitro_por_dia = parseFloat(result1 * 35)
+    var litros_por_dia = parseFloat(result1 * 0.035).toFixed(2)
+    var mililitro_por_dia = parseFloat(result1 * 35).toFixed(2)
 
     document.getElementById("litros_dia").value = litros_por_dia + " Litros"
     document.getElementById("mililitro_dia").value = mililitro_por_dia + " Mililitros"
